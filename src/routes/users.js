01db9ce7b5b54f7909aca04a720ace62d96a3a78
michaelroudnitski <michaelroudnitski@gmail.com>
2019-05-01 02:35:47 -0400

    added routes for accepting and rejecting friends

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,14 +5,6 @@ const User = require('../models/User');
 const Friendship = require('../models/Friendship');
 
 // Custom Middleware
-function getUser(req, res, next) {
-  User.findById(req.params.id, (err, user) => {
-    if (err) next(err);
-    req.user = user;
-    next();
-  });
-}
-
 function requiresLogin(req, res, next) {
   if (req.session && req.session.userId) return next();
   const err = new Error('User must be logged in.');
@@ -20,11 +12,15 @@ function requiresLogin(req, res, next) {
   return next(err);
 }
 
+// ROUTES
 users.post('/addfriend/:id', requiresLogin, (req, res, next) => {
   const contacts = {
     sender: req.session.userId,
     recipient: req.params.id
   };
+  // TODO: dont send request if already in accepted state
+  // TODO: only allow request to be resent if still in pending state after some time
+  // TODO: only allow request to be resent if in rejected state for 5 days
   Friendship.findOneAndUpdate(contacts,
     { status: 'pending' },
     { upsert: true, new: true },
@@ -38,6 +34,52 @@ users.get('/getfriends', requiresLogin, (req, res, next) => {
   User.getFriends(req.session.userId, (err, pendingReqs) => {
     if (err) return next(err);
     return res.send(pendingReqs);
+  });
+});
+
+users.post('/acceptfriend/:id', requiresLogin, (req, res, next) => {
+  Friendship.findById(req.params.id, (err, fRequest) => {
+    if (err) return next(err);
+    // if request was sent by someone other than recipient of request throw 401
+    // eslint-disable-next-line eqeqeq
+    if (fRequest.recipient != req.session.userId) {
+      const error = new Error('lacking credentials to accept friend request');
+      error.status = 401;
+      return next(error);
+    }
+    const updatedRequest = fRequest;
+    if (updatedRequest.status !== 'accepted') {
+      updatedRequest.status = 'accepted';
+      updatedRequest.save((saveErr) => {
+        if (saveErr) return next(saveErr);
+        return res.send('Added Friend!');
+      });
+    }
+    else return next(err);
+    return null;
+  });
+});
+
+users.post('/rejectfriend/:id', requiresLogin, (req, res, next) => {
+  Friendship.findById(req.params.id, (err, fRequest) => {
+    if (err) return next(err);
+    // if request was sent by someone other than recipient of request throw 401
+    // eslint-disable-next-line eqeqeq
+    if (fRequest.recipient != req.session.userId) {
+      const error = new Error('lacking credentials to reject friend request');
+      error.status = 401;
+      return next(error);
+    }
+    const updatedRequest = fRequest;
+    if (updatedRequest.status !== 'accepted') {
+      updatedRequest.status = 'rejected';
+      updatedRequest.save((saveErr) => {
+        if (saveErr) return next(saveErr);
+        return res.send('Rejected Friend Request');
+      });
+    }
+    else return next(err);
+    return null;
   });
 });
 
